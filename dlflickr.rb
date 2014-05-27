49b902255b3e4ddfaec96b149aa9d6a1088dbd59
Ryan Baumann <ryan.baumann@gmail.com>
2014-05-27 14:14:25 -0400

    Rescue 404, don't try if we already have the file

--- dlflickr.rb
+++ dlflickr.rb
@@ -4,14 +4,19 @@ require 'nokogiri'
 require 'open-uri'
 
 ARGF.each_line do |line|
-	line.chomp!
-	save_file = line.split('/')[-2..-1].join('_') + '.jpg'
-	doc = Nokogiri::HTML(open(line + "/sizes/o/"))
+  line.chomp!
+  save_file = line.split('/')[-2..-1].join('_') + '.jpg'
+  begin
+    unless File.exist?(save_file)
+      doc = Nokogiri::HTML(open(line + "/sizes/o/"))
 
-	doc.xpath('//div[@id="allsizes-photo"]/img/@src').each do |link|
-		puts "#{save_file}: #{line} => #{link}"
-		unless File.exist?(save_file)
-			`wget -O #{save_file} -c #{link}`
-		end
-	end
+      doc.xpath('//div[@id="allsizes-photo"]/img/@src').each do |link|
+        $stderr.puts "#{save_file}: #{line} => #{link}"
+        `wget -O #{save_file} -c #{link}`
+      end
+    end
+  rescue OpenURI::HTTPError => e
+    $stderr.puts line
+    $stderr.puts e.inspect
+  end
 end
