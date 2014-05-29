cee389c879003134303ccd525098b357d604ceca
Ryan Baumann <ryan.baumann@gmail.com>
2014-05-29 14:29:50 -0400

    Update spreadsheet with Flickr license info

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'nokogiri'
 gem 'google_drive'
+gem 'flickr.rb', :require => 'flickr'
--- google_spreadsheet_to_csv.rb
+++ google_spreadsheet_to_csv.rb
@@ -3,12 +3,43 @@
 require 'rubygems'
 require 'google_drive'
 require 'yaml'
+require 'flickr'
 
 config = YAML.load_file('.secrets.yml')
 session = GoogleDrive.login(config["email"], config["pass"])
 
+licenses = {
+  0 => "All Rights Reserved",
+  1 => "Attribution-NonCommercial-ShareAlike License",
+  2 => "Attribution-NonCommercial License",
+  3 => "Attribution-NonCommercial-NoDerivs License",
+  4 => "Attribution License",
+  5 => "Attribution-ShareAlike License",
+  6 => "Attribution-NoDerivs License",
+  7 => "No known copyright restrictions",
+  8 => "United States Government Work"
+}
+
 ws = session.spreadsheet_by_key(config["spreadsheet_key"]).worksheets[0]
 
 for row in 2..ws.num_rows
-  puts ws[row,2]
+  flickr_url = ws[row,2]
+
+  flickr_url.chomp!
+  flickr_url.sub!(/^http\:/,'https:')
+  flickr_url.sub!(/\/in\/.*/,'') # strip off /in/photostream/, /in/pool-* etc.
+  flickr_url.sub!(/\/$/,'') # strip trailing slash
+
+  photo_id = flickr_url.split('/').last.to_s
+  begin
+    flickr_photo = Flickr::Photo.new(photo_id, config["flickr_key"])
+    license = licenses[flickr_photo.license.to_i]
+    ws[row,6] = license
+  rescue RuntimeError => e
+    $stderr.puts e.inspect
+  end
+
+  puts flickr_url
 end
+
+ws.save()
