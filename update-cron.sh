062cc1b4cdb056f85c7f4cb44e978ba49acd5fec
Ryan Baumann <ryan.baumann@gmail.com>
2015-04-07 09:55:50 -0400

    Use bundler

--- update-cron.sh
+++ update-cron.sh
@@ -3,8 +3,9 @@
 # needed for ssh-agent auth under cron on OS X
 declare -x SSH_AUTH_SOCK=$( find /tmp/com.apple.launchd.*/Listeners -user $(whoami) -type s | head -1 )
 
-./google_spreadsheet_to_csv.rb > squeezes.csv
+bundle install
+bundle exec ./google_spreadsheet_to_csv.rb > squeezes.csv
 pushd squeezes
-../dlflickr.rb < ../squeezes.csv
+bundle exec ../dlflickr.rb < ../squeezes.csv
 popd
 rsync -avz -e ssh squeezes/ libdc3-dev-02.oit.duke.edu:/srv/data/IDEs/flickr
