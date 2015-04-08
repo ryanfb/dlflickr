fcdd1126b2b11f306fe7868e94d9394a8ae4e2ac
Ryan Baumann <ryan.baumann@gmail.com>
2015-04-08 17:56:29 -0400

    Add Flickr metadata archiving functionality (fixes #3)

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 dlflickr
 ========
 
-This repository contains a set of scripts which, used together, can automatically archive a set of Flickr URLs from a Google Sheet.
+This repository contains scripts for automatically archiving Flickr URLs.
 
-* `dlflickr.rb`: takes a list of newline-separated Flickr URLs as input and calls `wget` to download them to the current working directory
+* `dlflickr.rb`: takes a list of newline-separated Flickr URLs as input and calls `wget` to download them to the current working directory, optionally archiving Flickr metadata
 * `google_spreadsheet_to_csv.rb`: outputs Flickr URL's in a Google Sheet for input to `dlflickr.rb`, and updates a column in the Google Sheet with the image license
 * `update-cron.sh`: calls `google_spreadsheet_to_csv.rb` and `dlflickr.rb` then `rsync`s the images to a remote server
 
 You can use the components piecemeal, or use `update-cron.sh` on an automated basis if it meets your needs. Run `bundle install` to install gems first.
+
+`dlflickr.rb`
+-------------
+
+The core `dlflickr.rb` script tries to be as straightforward as possible. For each line of input, it tries to download the maximum resolution Flickr image at that URL using `wget`. If you want to get fancy, it also supports a `-a` flag for archiving Flickr metadata to a separate XML file [like this](https://gist.github.com/853a6e047aaa0063c8a9). This requires a [Flickr API key](https://www.flickr.com/services/api/keys/), which you can pass in with the `-f` flag or the `.secrets.yml` config file described below.
+
+`dlflickr.rb` currently *does not* overwrite existing files on subsequent invocations. Files are saved to `username-or-nsid_photo-id.ext` in the current working directory.
+
+`google_spreadsheet_to_csv.rb`
+------------------------------
 
 `google_spreadsheet_to_csv.rb` expects configuration variables to be stored in a file named `.secrets.yml`:
 
@@ -21,6 +31,19 @@ You can use the components piecemeal, or use `update-cron.sh` on an automated ba
     flickr_key: flickrapikey
     flickr_secret: flickrapikeysecret
 
+`update-cron.sh`
+----------------
+
 `update-cron.sh` expects your `rsync` destination in a file named `.rsync-dest`:
 
     remote-host.example.com:/srv/data/flickrbackup
+
+Related Tools
+-------------
+
+Based on the [Archive Team Wiki Flickr page](http://archiveteam.org/index.php?title=Flickr), here are some other Flickr backup tools that didn't quite meet my needs (still, they might meet yours):
+
+* [photobackup](https://hsivonen.fi/photobackup/) - designed to back up all photos and metadata for a single Flickr user
+* [flickrbackup](https://github.com/tiagovaz/flickrbackup) - backs up photos and metadata for a single Flickr user, and only supports writing Flickr metadata back into the original image EXIF
+* [parallel-flickr](https://github.com/straup/parallel-flickr) - backs up photos and metadata for a single Flickr user and and generates a database-backed website for re-hosting them
+
--- dlflickr.rb
+++ dlflickr.rb
@@ -2,23 +2,81 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'optparse'
+require 'yaml'
+require 'rest-client'
+require 'json'
+
+FLICKR_API_ENDPOINT = 'https://api.flickr.com/services/rest/'
+
+@rest_responses = {}
+
+def append_flickr_response(xml, params)
+  @rest_responses[params.hash] ||= RestClient.get FLICKR_API_ENDPOINT, {:params => params}
+  rest_response = @rest_responses[params.hash]
+  flickr_response_node = Nokogiri::XML "<flickrResponse method=\"#{params['method']}\"/>"
+  flickr_response_node.root.add_child Nokogiri::XML(rest_response.body.to_s).root
+  if ((flickr_response_node.xpath('/flickrResponse/rsp/@stat').first.value != 'ok') || rest_response.code != 200)
+    $stderr.puts "Error with Flickr request #{params.inspect}"
+    $stderr.puts rest_response.inspect
+  end
+  xml.root.add_child flickr_response_node.root
+
+  return xml
+end
+
+config = {}
+
+config_yaml = File.join(File.dirname(File.expand_path(__FILE__)),'.secrets.yml')
+if File.exist?(config_yaml)
+  config = config.merge(YAML.load_file(config_yaml))
+end
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: dlflickr.rb [options]"
+  opts.on('-a','--[no-]archive','Archive Flickr metadata') {|a| config[:archive] = a}
+  opts.on('-fKEY','--flickr-api-key=KEY','Flickr API key') {|f| config['api_key'] = f}
+end.parse!
 
 ARGF.each_line do |line|
   line.chomp!
   line.sub!(/^http\:/,'https:')
   line.sub!(/\/in\/.*/,'') # strip off /in/photostream/, /in/pool-* etc.
-  save_file = line.split('/')[-2..-1].join('_') + '.jpg'
+  save_file_basename = line.split('/')[-2..-1].join('_')
+  save_file_image = save_file_basename + '.jpg'
+  save_file_metadata = save_file_basename + '.xml'
+  photo_id = line.split('/')[-1]
+  # download Flickr image
   begin
-    unless File.exist?(save_file)
+    unless File.exist?(save_file_image)
       doc = Nokogiri::HTML(open(line + "/sizes/o/"))
 
       doc.xpath('//div[@id="allsizes-photo"]/img/@src').each do |link|
-        $stderr.puts "#{save_file}: #{line} => #{link}"
-        `wget -O #{save_file} -c #{link}`
+        $stderr.puts "#{save_file_image}: #{line} => #{link}"
+        `wget -O #{save_file_image} -c #{link}`
       end
     end
   rescue OpenURI::HTTPError => e
     $stderr.puts line
     $stderr.puts e.inspect
   end
+  # download Flickr metadata
+  if (config[:archive] && !File.exist?(save_file_metadata))
+    xml = Nokogiri::XML '<flickrMetadata/>'
+    # flickr.photos.getInfo
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.photos.getInfo', 'photo_id' => photo_id})
+    # flickr.photos.licenses.getInfo
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.photos.licenses.getInfo'})
+    # flickr.photos.comments.getList
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.photos.comments.getList', 'photo_id' => photo_id})
+    # flickr.photos.getFavorites
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.photos.getFavorites', 'photo_id' => photo_id, 'per_page' => 50})
+    # flickr.photos.getAllContexts
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.photos.getAllContexts', 'photo_id' => photo_id})
+    # flickr.people.getInfo
+    owner_nsid = xml.xpath('//photo/owner/@nsid').first ? xml.xpath('//photo/owner/@nsid').first.value : line.split('/')[-2]
+    xml = append_flickr_response(xml, {'api_key' => config['flickr_key'], 'method' => 'flickr.people.getInfo', 'user_id' => owner_nsid})
+    $stderr.puts "#{save_file_metadata}: #{File.write(save_file_metadata, xml.to_s)} bytes written"
+  end
+
 end
