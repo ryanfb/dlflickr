47394c53b2eb14f9213f5daf9fce0039f5833afd
Ryan Baumann <ryan.baumann@gmail.com>
2017-06-23 11:33:58 -0400

    Make sure we only parse flickr URLs in the URL column

--- google_spreadsheet_to_csv.rb
+++ google_spreadsheet_to_csv.rb
@@ -29,23 +29,24 @@ for row in 2..ws.num_rows
   flickr_url = ws[row,config["spreadsheet_flickr_column"]]
 
   flickr_url.chomp!
-  flickr_url.sub!(/^http\:/,'https:')
-  flickr_url.sub!(/\/in\/.*/,'') # strip off /in/photostream/, /in/pool-* etc.
-  flickr_url.sub!(/\/$/,'') # strip trailing slash
+  if URI(flickr_url).host.split('.')[-2..-1].join('.') == 'flickr.com'
+    flickr_url.sub!(/^http\:/,'https:')
+    flickr_url.sub!(/\/in\/.*/,'') # strip off /in/photostream/, /in/pool-* etc.
+    flickr_url.sub!(/\/$/,'') # strip trailing slash
 
-  photo_id = flickr_url.split('/').last.to_s
-  unless config["spreadsheet_license_column"].nil?
-    begin
-      flickr_photo = Flickr::Photo.new(photo_id, config["flickr_key"])
-      license = licenses[flickr_photo.license.to_i]
-      ws[row,config["spreadsheet_license_column"]] = license
-    rescue RuntimeError => e
-      $stderr.puts "Error processing license for #{photo_id}:"
-      $stderr.puts e.inspect
+    photo_id = flickr_url.split('/').last.to_s
+    unless config["spreadsheet_license_column"].nil?
+      begin
+        flickr_photo = Flickr::Photo.new(photo_id, config["flickr_key"])
+        license = licenses[flickr_photo.license.to_i]
+        ws[row,config["spreadsheet_license_column"]] = license
+      rescue RuntimeError => e
+        $stderr.puts "Error processing license for #{photo_id}:"
+        $stderr.puts e.inspect
+      end
     end
+    puts flickr_url
   end
-
-  puts flickr_url
 end
 
 ws.save()
